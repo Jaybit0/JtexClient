dd7d3bbc37564dc819f8b1af1e18c1862f8490be
Jaybit0 <jannik.lindemann@freenet.de>
2023-05-11 22:42:06 +0200

    Bugfix

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@
 rem Check if a process ID was provided as an argument
 IF NOT "%1"=="" (
   rem Get the process ID from the command line argument
-  set PID=%1
+  set "PID=%1"
 
   rem Kill the process with the specified process ID
   echo Killing process with PID %PID%
