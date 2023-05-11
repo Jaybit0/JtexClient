c10d8e0bf32969fd697cf33e1870aea6427269e8
Jaybit0 <jannik.lindemann@freenet.de>
2023-05-11 22:55:25 +0200

    test

--- setup.bat
+++ setup.bat
@@ -1,13 +1,13 @@
 @echo off
 
-rem Check if a process ID was provided as an argument
-IF NOT "%1"=="" (
-  rem Get the process ID from the command line argument
-  set test=%1
+rem Get the process ID from the command line argument
+set "PID=%1"
 
+rem Check if a process ID was provided as an argument
+IF NOT "%PID%"=="" (
   rem Kill the process with the specified process ID
-  echo Killing process with PID %1
-  taskkill /F /PID %test%
+  echo Killing process with PID %PID%
+  taskkill /F /PID %PID%
 
   rem Add a small delay to ensure the process is terminated
   timeout /t 1 /nobreak >nul
