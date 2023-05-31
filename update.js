f4c0ad3af66dd52d74f6188374aa651f8b584d5b
Jaybit0 <jannik.lindemann3@gmail.com>
2023-05-31 15:12:59 +0200

    Fix: #2

--- update.js
+++ update.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 function update(args) {
+    switch(process.platform){
+        case "darwin":
+        case "linux":
+            // Check if the user is root
+            if (process.getuid() !== 0) {
+                console.error('You must run this script as root (sudo jtex update <branch/version/commit>)');
+                return;
+            }
+            break;
+        default:
+    }
+
     if (args.length < 2) {
         console.log("Expecting the commit hash or tag as argument.");
         return;
@@ -15,14 +27,21 @@ function update(args) {
         json.dependencies["jtex-core"] = "git://github.com/Jaybit0/JtexCore.git#" + args[1];
     }
     fs.writeFileSync(path.join(__dirname, "package.json"), JSON.stringify(json, null, 4));
-    exec("npm update jtex-core", (err, stdout, stderr) => {
-        if (err) {
-            console.log("An error appeared while updating the package.");
+    const updateScript = spawn("npm", ["update", "jtex-core"], {
+        cwd: __dirname,
+        stdio: "inherit"
+    });
+
+    updateScript.on('error', (error) => {
+        console.error("Error running npm update:", error);
+    });
+
+    updateScript.on('exit', (code) => {
+        if (code === 0) {
+          console.log('Update completed successfully');
+        } else {
+          console.error('Update failed with error code:', code);
         }
-        console.log(stdout);
-        console.log(stderr);
-        if (!err)
-            console.log("Update successful!");
     });
 }
 
